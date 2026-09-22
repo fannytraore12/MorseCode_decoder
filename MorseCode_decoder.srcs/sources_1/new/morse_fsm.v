3a0be90db9fbac6080ad88c0c4891a0777af997f
fannytraore12 <fannytraore002@gmail.com>
2026-09-22 16:42:26 -0400

    Created morsecode Fsm cycle

---
+++ MorseCode_decoder.srcs/sources_1/new/morse_fsm.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/22/2026 02:47:20 PM
+// Design Name: 
+// Module Name: morse_fsm
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module morse_fsm(
+
+    );
+endmodule
